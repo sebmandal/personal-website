1f3c1c55056ce321bf9a0ab90f8670af3b81cefd
sebmandal <sebastian.mandal@icloud.com>
2021-11-02 20:15:06 +0100

    hopefully this will fix the cookies

---
+++ src/routes/reset-cookies.ts
@@ -0,0 +1,29 @@
+import Route from '../core/route'
+import Express from 'express'
+
+const script = (req: Express.Request, res: Express.Response) => {
+    // grab every cookie and delete it
+    Object.keys(req.cookies).forEach((key) => {
+        res.clearCookie(key)
+    })
+
+    Object.keys(req.signedCookies).forEach((key) => {
+        res.clearCookie(key)
+    })
+
+    res.redirect('/')
+}
+
+export default class ResetCookiesGet extends Route {
+    /**
+     * @inheritdoc
+     * @param {string} path
+     * @param {string} method
+     * @param {function} callback
+     * @returns {void}
+     **/
+
+    constructor() {
+        super('/reset-cookies', 'get', script)
+    }
+}
